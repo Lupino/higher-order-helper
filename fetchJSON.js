d0b33d41560abd9ae96ac59bb1685df8a513b237
Lupino <lmjubuntu@gmail.com>
2017-12-20 16:57:00 +0800

    update fetchJSON

--- fetchJSON.js
+++ fetchJSON.js
@@ -8,13 +8,13 @@ var _regenerator = require('babel-runtime/regenerator');
 
 var _regenerator2 = _interopRequireDefault(_regenerator);
 
-var _typeof2 = require('babel-runtime/helpers/typeof');
-
-var _typeof3 = _interopRequireDefault(_typeof2);
-
 var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _typeof2 = require('babel-runtime/helpers/typeof');
+
+var _typeof3 = _interopRequireDefault(_typeof2);
 
 var _lodash = require('lodash.keys');
 
@@ -26,9 +26,28 @@ var _fetch2 = _interopRequireDefault(_fetch);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function preprocess(data) {
+  if (data.err) {
+    throw new Error(data.err);
+  }
+  if (Array.isArray(data)) {
+    return data;
+  }
+  if ((typeof data === 'undefined' ? 'undefined' : (0, _typeof3.default)(data)) === 'object') {
+    var k = (0, _lodash2.default)(data);
+    if (k.length === 1) {
+      return data[k[0]];
+    } else {
+      return data;
+    }
+  }
+  return data;
+}
+
 exports.default = function () {
   var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(url, options) {
-    var rsp, data, k, err;
+    var rsp, data, _data, parsed;
+
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -42,7 +61,7 @@ exports.default = function () {
             rsp = _context.sent;
 
             if (!/application\/json/.test(rsp.headers.get('content-type'))) {
-              _context.next = 21;
+              _context.next = 12;
               break;
             }
 
@@ -51,57 +70,29 @@ exports.default = function () {
 
           case 8:
             data = _context.sent;
+            return _context.abrupt('return', preprocess(data));
 
-            if (!data.err) {
-              _context.next = 11;
-              break;
-            }
-
-            throw new Error(data.err);
-
-          case 11:
-            if (!Array.isArray(data)) {
-              _context.next = 13;
-              break;
-            }
-
-            return _context.abrupt('return', data);
-
-          case 13:
-            if (!((typeof data === 'undefined' ? 'undefined' : (0, _typeof3.default)(data)) === 'object')) {
-              _context.next = 20;
-              break;
-            }
-
-            k = (0, _lodash2.default)(data);
-
-            if (!(k.length === 1)) {
-              _context.next = 19;
-              break;
-            }
-
-            return _context.abrupt('return', data[k[0]]);
-
-          case 19:
-            return _context.abrupt('return', data);
-
-          case 20:
-            return _context.abrupt('return', data);
-
-          case 21:
-            _context.next = 23;
+          case 12:
+            _context.next = 14;
             return rsp.text();
 
-          case 23:
-            err = _context.sent;
-            throw new Error(err);
+          case 14:
+            _data = _context.sent;
+            _context.prev = 15;
+            parsed = JSON.parse(_data);
+            return _context.abrupt('return', preprocess(_data));
 
-          case 25:
+          case 20:
+            _context.prev = 20;
+            _context.t0 = _context['catch'](15);
+            throw new Error(_data);
+
+          case 23:
           case 'end':
             return _context.stop();
         }
       }
-    }, _callee, this);
+    }, _callee, this, [[15, 20]]);
   }));
 
   function fetchJSON(_x, _x2) {
--- src/fetchJSON.js
+++ src/fetchJSON.js
@@ -1,29 +1,38 @@
 import keys from 'lodash.keys';
 import fetch from './fetch';
 
+function preprocess(data) {
+  if (data.err) {
+    throw new Error(data.err);
+  }
+  if (Array.isArray(data)) {
+    return data;
+  }
+  if (typeof data === 'object') {
+    const k = keys(data);
+    if (k.length === 1) {
+      return data[k[0]];
+    } else {
+      return data;
+    }
+  }
+  return data;
+}
+
 export default async function fetchJSON(url, options) {
   options = options || {};
   options['Accept'] = 'application/json';
   const rsp = await fetch(url, options);
   if (/application\/json/.test(rsp.headers.get('content-type'))) {
     const data = await rsp.json();
-    if (data.err) {
-      throw new Error(data.err);
+    return preprocess(data);
+  } else {
+    const data = await rsp.text();
+    try {
+      const parsed = JSON.parse(data);
+      return preprocess(data);
+    } catch (e) {
+      throw new Error(data);
     }
-    if (Array.isArray(data)) {
-      return data;
-    }
-    if (typeof data === 'object') {
-      const k = keys(data);
-      if (k.length === 1) {
-        return data[k[0]];
-      } else {
-        return data;
-      }
-    }
-    return data;
   }
-
-  const err = await rsp.text();
-  throw new Error(err);
 }
